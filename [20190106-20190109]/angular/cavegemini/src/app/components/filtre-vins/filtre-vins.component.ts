e00f33bb292f0dfed1070eea57edb8cf4f28f12d
Stagiaire <Stagiaire@INSTRUCT.edu-lol.loc>
2020-01-09 10:33:33 +0100

    filtrage par terroir (coté angular)

--- [20190106-20190109]/angular/cavegemini/src/app/components/filtre-vins/filtre-vins.component.ts
+++ [20190106-20190109]/angular/cavegemini/src/app/components/filtre-vins/filtre-vins.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Terroir } from 'src/app/metier/terroir';
 
 @Component({
   selector: 'app-filtre-vins',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filtre-vins.component.css']
 })
 export class FiltreVinsComponent implements OnInit {
+  @Input() terroirs : Terroir[];
+  @Output() onTerroirIdClick  = new EventEmitter<number>();
+
+  choixTerroir : number;
 
   constructor() { }
 
   ngOnInit() {
+    this.choixTerroir = 0;
   }
 
+  onChoixTerroir(event : number) {
+    //console.log(event);
+    this.onTerroirIdClick.emit(event);
+  }
 }
